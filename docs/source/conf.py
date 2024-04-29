01699db4ac7ec34981f6a41ce7de7a5373ba06ad
Mikej81 <stumpj@gmail.com>
2024-04-29 11:02:23 -0400

    fix conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,6 @@ release = '2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'myst_parser'
-]
-
 templates_path = ['_templates']
 exclude_patterns = []
 
