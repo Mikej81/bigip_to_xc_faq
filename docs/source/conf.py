d71d51208097a1ed40a7459d09f1e31425247315
Mikej81 <stumpj@gmail.com>
2024-04-29 11:07:55 -0400

    f5 theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+import f5_sphinx_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -20,5 +21,18 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html4_writer = True
+html_theme = "f5_sphinx_theme"
+html_theme_path = f5_sphinx_theme.get_html_theme_path()
+html_sidebars = {"**": ["searchbox.html", "localtoc.html", "globaltoc.html"]}
+html_theme_options = {
+    "site_name": "Community Training Classes & Labs",
+    "next_prev_link": True
+}
+html_codeblock_linenos_style = 'table'
+#html_context = {"github_url": github_repo}
+
+html_last_updated_fmt = "%Y-%m-%d %H:%M:%S"
+
+#extlinks = {"issues": (("%s/issues/%%s" % github_repo), "issue ")}
 html_static_path = ['_static']
